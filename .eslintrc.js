31eb1d1534aaaa49a199aa78baa705b01a95dd2c
pasqualevitiello <pasqualevitiello@gmail.com>
2020-07-08 15:38:15 +0200

    turn off some eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,8 @@ module.exports = {
     "env": {
       "browser": true,
       "node": true
-    }
+    },
+    "rules": {
+      "import/no-extraneous-dependencies": ["error", { "devDependencies": true }],
+    },
 };
