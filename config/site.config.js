6cc46f01184f5e614c8706bb7316e881a712c438
pasqualevitiello <pasqualevitiello@gmail.com>
2020-07-08 15:18:07 +0200

    remove questions at the beginning

--- config/site.config.js
+++ config/site.config.js
@@ -9,10 +9,10 @@ if (!ROOT) {
 
 const config = {
   // Your website's name, used for favicon meta tags
-  site_name: 'Static Site Boilerplate',
+  site_name: 'Open PRO template',
 
   // Your website's description, used for favicon meta tags
-  site_description: 'A modern boilerplate for static website development',
+  site_description: 'A Cruip template',
 
   // Your website's URL, used for sitemap
   site_url: 'https://cruip.com',
