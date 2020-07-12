021e0b128c89d042a3fa49a4b86bf2e371eef5e0
pasqualevitiello <pasqualevitiello@gmail.com>
2020-07-12 17:02:08 +0200

    mc

--- config/site.config.js
+++ config/site.config.js
@@ -9,7 +9,7 @@ if (!ROOT) {
 
 const config = {
   // Your website's name, used for favicon meta tags
-  site_name: 'Open PRO template',
+  site_name: 'Alpha HTML Template',
 
   // Your website's description, used for favicon meta tags
   site_description: 'A Cruip template',
